95ecf87c76086c38ed184435f39e6382da97a00d
unknown <naocenar@gmail.com>
2023-05-03 15:02:44 +0800

    fixing responsive header and aside

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -2,6 +2,7 @@
 import { Data } from "@/app/context"
 import { useContext } from "react"
 import { useRouter } from 'next/navigation';
+import Link from "next/link";
 
 export default function Aside() {
   const {aside, loggedIn, setLoggedIn} = useContext(Data);
@@ -16,11 +17,23 @@ export default function Aside() {
     {
       loggedIn ? <>
         <button onClick={signOut}>Log out</button>
+        <p>
+          <Link href='/search'>Search</Link>
+        </p>
         <div className="shops">
           
         </div>
       </> : <>
         <p>Not Log in</p>
+        <p>
+          <Link href='/search'>Search</Link>
+        </p>
+        <p>
+          <Link href='/register'>Register</Link>
+        </p>
+        <p>
+          <Link href='/login'>Login</Link>
+        </p>
       </>
     }
   </aside>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,40 @@
 'use client'
 import Link from "next/link"
 import { Data } from "@/app/context"
-import { useContext } from "react"
-import { AiOutlineMenu } from 'react-icons/ai'
+import { useContext, useEffect, useState } from "react"
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai'
 
 export default function Header() {
   const {loggedIn, aside} = useContext(Data);
-  
+  const [showSearchBar, setShowSearchBar] = useState(false)
+
+  useEffect(() => {
+    if (new URL(window.location.href).pathname.toString() === '/search'){
+      setShowSearchBar(true)
+    } else {
+      setShowSearchBar(false)
+    }
+  }, [])
+
   return <header>
     <Link href='/' className="logo">FOOD APP</Link>
     <>
       {
         !loggedIn ? <>
-          <Link href='/register'>Register</Link>
-          <Link href='/login'>Login</Link>
+          <Link href='/register' className="disappeanOn500Px">Register</Link>
+          <Link href='/login' className="disappeanOn500Px">Login</Link>
         </> : <>
           {
-            (new URL(window.location.href)).pathname.toString() !== '/search' && (
+            showSearchBar && (
               <>
                 <input className="search-input" type="text" placeholder='Search food' />
-                <button className="search-btn">Search</button>
+                <button className="search-btn">
+                  O
+                </button>
               </>
             )
           }
-          <Link href='/search'>Search</Link>
+          <Link href='/search' className="disappeanOn500Px">Search</Link>
         </>
       }
       <span className="sidebar-btn" onClick={() => aside.current.classList.toggle('showAside')}>
